68519040d5d3bff73b29a7fa63d4dc4ba15ebed2
Laysiks <kacperwierciak2@gmail.com>
2015-02-21 21:10:28 +0000

    Do zrobienia:(..)

--- Gracze-ZapisDB/c.lua
+++ Gracze-ZapisDB/c.lua
@@ -1,2 +1,10 @@
 --[[Autor:Laysiks]]--
 --[[Zakaza zmiany autora]]--
+
+
+--[[
+Po stronie klienta zrobione będzie:
+    * Teleportowanie się do graczy o danym ID,
+    * Namierzanie/Oznaczanie blipem graczy,
+    * Kilka Opcji dla Administracji.
+]]--
